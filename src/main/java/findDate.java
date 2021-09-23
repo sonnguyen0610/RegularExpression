070a72401264e9c97b0420cabb27f1158489d0ba
sonnguyen0610 <17130194@st.hcmuaf.edu.vn>
2021-09-23 10:51:24 +0700

    Initial commit

---
+++ src/main/java/findDate.java
@@ -0,0 +1,26 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class findDate {
+    public static void main(String[] args) {
+        //Bai 1
+        String txt="Hello java regex 2-12-2018, hello world 12/12/2018";
+        Pattern pattern = Pattern.compile("\\d{1,2}[-/]\\d{1,2}[-/]\\d{4}");
+        Matcher matcher = pattern.matcher(txt);
+
+        System.out.println("Ngày tháng trong chuỗi text1: " + txt);
+        while (matcher.find()) {
+            System.out.println(txt.substring(matcher.start(), matcher.end()));
+        }
+
+        //Bai 2
+        String regex="Hello java regex son@gmail.com, hello world sonnguyen@gmail.com.com";
+        Pattern p = Pattern.compile("\\w+(@{1})gmail(\\.[a-z]{2,}){1,2}");
+        Matcher m = p.matcher(regex);
+
+        System.out.println("Các email có trong chuỗi: " + regex);
+        while (m.find()) {
+            System.out.println(regex.substring(m.start(), m.end()));
+        }
+    }
+}
